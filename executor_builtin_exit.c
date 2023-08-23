dbd150e17c137683362ff07197fc6a4c0187b3da
Ezralx <ezrateshome.alx@gmail.com>
2023-08-23 08:20:54 -0700

    Fix Exit: Correct error codes

--- executor_builtin_exit.c
+++ executor_builtin_exit.c
@@ -1,5 +1,4 @@
 #include "shell.h"
-#include <stdlib.h>
 
 /**
  * execute_builtin_exit - executes the builtin command 'exit'
@@ -15,7 +14,7 @@ void execute_builtin_exit(shell_t *sh)
 		{
 			_dprintf(STDERR_FILENO, "%s: %d: exit: Illegal number: %s\n",
 					sh->prg_name, sh->line_no, sh->cmd_av[1]);
-			sh->exit_code = EXIT_FAILURE;
+			sh->exit_code = CMD_EXIT_FAILURE;
 			return;
 		}
 		sh->exit_code = _atoi(sh->cmd_av[1]);
